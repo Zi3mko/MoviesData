12d9aaa2aaa476bd6655ff79af8b74a2a072881e
DominikSledzik <dominiksledzik1@gmail.com>
2022-01-03 22:17:52 +0100

    Final Fixes, changed the csvToJson.py code to actually change the csv to appropriate json format.

--- csvToJson.py
+++ csvToJson.py
@@ -1,13 +1,11 @@
-import json
+import json 
 import csv
 
-data ={}
-
-with open("Movies.csv",encoding='utf-8') as csvFile:
-    csvReader = csv.DictReader(csvFile)
-    for rows in csvReader:
-        key = rows['movie_id']
-        data[key] = rows
-
-with open("movies.json",'w',encoding='utf-8') as jsonf:
-    jsonf.write(json.dumps(data,indent=4))
+with open('./datasets/Movies.csv','r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    next(reader)
+    data = {"movies": []}
+    for row in reader:
+        data["movies"].append({"Title": row[0], "Director": row[1], "Cast": row[2], "ReleaseYear": row[3], "Duration": row[4], "Categories": row[5], "Description": row[6]})
+with open("movies.json",'w') as f:
+    json.dump(data,f, indent=4)
